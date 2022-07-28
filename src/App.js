8c061c06f70acaaae3dd9c66872a6052b2eb6638
danhNg <danh.nguyenthanh102@gmail.com>
2022-07-29 01:36:50 +0700

    end

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
+import BaiTapThucHanhLayout from "./BaiTapLayoutComponent/BaiTapThucHanhLayout";
 
 function App() {
   return (
     <div className="App">
-
+<BaiTapThucHanhLayout/>
     </div>
   );
 }
