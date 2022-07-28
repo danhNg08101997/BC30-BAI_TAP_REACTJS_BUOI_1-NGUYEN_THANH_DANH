426d6fc05e645a823dca583c462efd25f89e4b42
danhNg <danh.nguyenthanh102@gmail.com>
2022-07-29 01:55:25 +0700

    sua css

--- src/BaiTapLayoutComponent/Body.jsx
+++ src/BaiTapLayoutComponent/Body.jsx
@@ -18,11 +18,12 @@ export default class Body extends Component {
                 <section className='item pt-4'>
                     <div className="container px-lg-5">
                         <div className="row gx-lg-5">
-                        <div className="col-lg-6 col-xxl-4 mb-5">
+                            <div className="col-lg-6 col-xxl-4 mb-5">
                                 <div className="card bg-light h-100 border-0">
                                     <div className="card-body text-center p-4 p-lg5 pt-0 pt-lg-0">
-                                        <div className="feature bg-primary bg-gradient text-white mb-4 mt-n4 rounded-3">
-                                        <i class="bi bi-collection"></i>                                        </div>
+                                        <div className="feature bg-primary bg-gradient text-white mb-4 rounded-3">
+                                            <i class="bi bi-collection"></i>
+                                        </div>
                                         <h2 className='fs-4 fw-bold'>Fresh new layout</h2>
                                         <p className='mb-0'>With Bootstrap 5, we've created a fresh new layout for this template!</p>
                                     </div>
@@ -31,8 +32,8 @@ export default class Body extends Component {
                             <div className="col-lg-6 col-xxl-4 mb-5">
                                 <div className="card bg-light h-100 border-0">
                                     <div className="card-body text-center p-4 p-lg5 pt-0 pt-lg-0">
-                                        <div className="feature bg-primary bg-gradient text-white mb-4 mt-n4 rounded-3">
-                                        <i class="bi bi-cloud-download"></i>
+                                        <div className="feature bg-primary bg-gradient text-white mb-4 rounded-3">
+                                            <i class="bi bi-cloud-download"></i>
                                         </div>
                                         <h2 className='fs-4 fw-bold'>Free to download</h2>
                                         <p className='mb-0'>As always, Start Bootstrap has a powerful collectin of free templates.</p>
@@ -42,8 +43,8 @@ export default class Body extends Component {
                             <div className="col-lg-6 col-xxl-4 mb-5">
                                 <div className="card bg-light h-100 border-0">
                                     <div className="card-body text-center p-4 p-lg5 pt-0 pt-lg-0">
-                                        <div className="feature bg-primary bg-gradient text-white mb-4 mt-n4 rounded-3">
-                                        <i class="bi bi-card-heading"></i>
+                                        <div className="feature bg-primary bg-gradient text-white mb-4 rounded-3">
+                                            <i class="bi bi-card-heading"></i>
                                         </div>
                                         <h2 className='fs-4 fw-bold'>Jumbotron hero header</h2>
                                         <p className='mb-0'>The heroic part of this template is the jumbotron hero header!</p>
@@ -53,8 +54,8 @@ export default class Body extends Component {
                             <div className="col-lg-6 col-xxl-4 mb-5">
                                 <div className="card bg-light h-100 border-0">
                                     <div className="card-body text-center p-4 p-lg5 pt-0 pt-lg-0">
-                                        <div className="feature bg-primary bg-gradient text-white mb-4 mt-n4 rounded-3">
-                                        <i class="bi bi-bootstrap"></i>
+                                        <div className="feature bg-primary bg-gradient text-white mb-4 rounded-3">
+                                            <i class="bi bi-bootstrap"></i>
                                         </div>
                                         <h2 className='fs-4 fw-bold'>Feature boxes</h2>
                                         <p className='mb-0'>We've created some custom feature boxes using Bootstrap icons!</p>
@@ -64,8 +65,8 @@ export default class Body extends Component {
                             <div className="col-lg-6 col-xxl-4 mb-5">
                                 <div className="card bg-light h-100 border-0">
                                     <div className="card-body text-center p-4 p-lg5 pt-0 pt-lg-0">
-                                        <div className="feature bg-primary bg-gradient text-white mb-4 mt-n4 rounded-3">
-                                        <i class="bi bi-code"></i>
+                                        <div className="feature bg-primary bg-gradient text-white mb-4 rounded-3">
+                                            <i class="bi bi-code"></i>
                                         </div>
                                         <h2 className='fs-4 fw-bold'>Simple clean code</h2>
                                         <p className='mb-0'>We keep our dependencies up to date and squash bugs as they come!</p>
@@ -75,8 +76,8 @@ export default class Body extends Component {
                             <div className="col-lg-6 col-xxl-4 mb-5">
                                 <div className="card bg-light h-100 border-0">
                                     <div className="card-body text-center p-4 p-lg5 pt-0 pt-lg-0">
-                                        <div className="feature bg-primary bg-gradient text-white mb-4 mt-n4 rounded-3">
-                                        <i class="bi bi-patch-check"></i>
+                                        <div className="feature bg-primary bg-gradient text-white mb-4 rounded-3">
+                                            <i class="bi bi-patch-check"></i>
                                         </div>
                                         <h2 className='fs-4 fw-bold'>A name you trust</h2>
                                         <p className='mb-0'>Start Bootstrap has been the leader in free Bootstrap templates since 2013!</p>
